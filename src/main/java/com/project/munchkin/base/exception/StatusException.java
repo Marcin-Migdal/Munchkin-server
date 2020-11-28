a0947f7392062211b6a67aaa9483f987cb0de40d
morti132 <marcin.migdal1996@gmail.com>
2020-11-28 14:41:19 +0100

    tests and changes in controller

---
+++ src/main/java/com/project/munchkin/base/exception/StatusException.java
@@ -0,0 +1,9 @@
+package com.project.munchkin.base.exception;
+
+public class StatusException extends RuntimeException {
+
+    public StatusException(String message) {
+        super(message);
+    }
+
+}
